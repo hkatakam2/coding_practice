b15b2fc7bacfced7454f669cabe5933d072ba973
Nagami2 <nagababukatakam2@gmail.com>
2025-12-22 12:25:38 -0500

    augment input, to handle edge cases

--- 84.histogram_area.py
+++ 84.histogram_area.py
@@ -8,26 +8,19 @@ from typing import List
 
 # idea: use stack; If the current bar is smaller, pop the tall ones and calculate area
 def largest_rectangle(heights: List[int]) -> int:
+    heights.append(0)  # to force the stack to empty at the end
     max_area = 0
-    stack = []
+    stack = [-1]  # to handle the left boundary of the first bar
 
     for i, curr_height in enumerate(heights):
-        while stack and heights[stack[-1]] > curr_height:
+        while stack[-1] != -1 and heights[stack[-1]] > curr_height:
+            # 1. pop top element, height of rectangle
             height = heights[stack.pop()]
 
-            if not stack:
-                width = i  # extends from 0 to i
-            else:
-                width = i - stack[-1] - 1
+            # 2. right boundary is the current index i
+            # 3. left boundary is the "new" top of the stack
+            width = i - stack[-1] - 1
+
             max_area = max(max_area, height * width)
         stack.append(i)
-
-    # process remaining bars in the stack
-    while stack:
-        height = heights[stack.pop()]
-        if not stack:
-            width = len(heights)  # extend to the very end
-        else:
-            width = len(heights) - stack[-1] - 1
-        max_area = max(max_area, height * width)
     return max_area
