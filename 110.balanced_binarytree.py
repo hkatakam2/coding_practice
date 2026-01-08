fd97efb03b96c0a63d3378d6e2273d5f64e46189
Nagami2 <nagababukatakam2@gmail.com>
2026-01-08 11:10:07 -0500

    post order traversal with rich return values

--- 110.balanced_binarytree.py
+++ 110.balanced_binarytree.py
@@ -1,17 +1,21 @@
-from functools import cache
-
-
-def is_balanced(self, root):
-    @cache
-    def height(node):
+def is_balanced(self, root: Optional[TreeNode]) -> bool:
+    def dfs(node):
         if not node:
-            return 0
-        return 1 + max(height(node.left), height(node.right))
+            return (True, 0)
 
-    if not root:
-        return True
+        # ask left child for the report
+        left_balanced, left_height = dfs(node.left)
+        # ask right child for the report
+        right_balanced, right_height = dfs(node.right)
+        # process the results
+        # if either child is unbalanced, I am unbalanced too
+        if not left_balanced or not right_balanced:
+            return (False, 0)  # height does not matter
+        # if children are ok, check my own balance
+        if abs(left_height - right_height) > 1:
+            return (False, 0)
+        # if everything is ok, return True and my height
+        return (True, 1 + max(left_height, right_height))
 
-    if abs(height(root.left) - height(root.right)) > 1:
-        return False
-    else:
-        return self.is_balanced(root.left) and self.is_balanced(root.right)
+    balanced, height = dfs(root)
+    return balanced
