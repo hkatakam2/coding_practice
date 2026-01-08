8a666ee1b87da373fe2277f7de87de36d9b8de2b
Nagami2 <nagababukatakam2@gmail.com>
2026-01-08 10:04:34 -0500

    using inbuilt cache

--- 110.balanced_binarytree.py
+++ 110.balanced_binarytree.py
@@ -1,22 +1,15 @@
+from functools import cache
+
+
 def is_balanced(self, root):
-    # 1. create a cache to store heights we've already calculated
-    self.height_cache = {}
-
-    if not root:
-        return True
-
+    @cache
     def height(node):
         if not node:
             return 0
+        return 1 + max(height(node.left), height(node.right))
 
-        # before calculating, check if already exists
-        if node in self.height_cache:
-            return self.height_cache[node]
-
-        # calculate and store it
-        h = 1 + max(height(node.left), height(node.right))
-        self.height_cache[node] = h
-        return h
+    if not root:
+        return True
 
     if abs(height(root.left) - height(root.right)) > 1:
         return False
